12e8ed8babac2ae48eb9fcc214f76ba80dc956f9
Dylan Clark <dylanmcdanielclark@gmail.com>
2021-02-13 11:18:25 -0800

    working, simple repl

---
+++ src/err.cpp
@@ -0,0 +1,18 @@
+#include <loxpp_pch.h>
+
+#include <err.h>
+
+namespace Loxpp::Err {
+UsageException::UsageException(Type type)
+    : m_type(type), m_message(buildMessage(type)) {}
+
+const char *UsageException::what() const noexcept { return m_message.c_str(); }
+
+/*static*/ std::string UsageException::buildMessage(Type type) {
+  std::ostringstream stream;
+  stream << "Usage error:\n\t" << m_whatStrings[static_cast<size_t>(type)]
+         << std::endl;
+  return stream.str();
+}
+
+} // namespace Loxpp::Err
--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,43 @@
 ﻿#include <loxpp_pch.h>
 
+#include <err.h>
+
+using namespace Loxpp;
+
 // FD
 void runFile(std::string filePath);
 void runPrompt();
 
-int main(int argc, char **argv) {
-  if (argc > 2) {
-    std::cout << "Usage: jlox [script]" << std::endl;
-    return -1;
-  } else if (argc == 2) {
-    runFile(std::string(argv[argc]));
-  } else {
-    runPrompt();
+int main(int argc, char **argv) noexcept {
+  try {
+    if (argc > 2) {
+      std::cout << "Usage: jlox [script]" << std::endl;
+      return -1;
+    } else if (argc == 2) {
+      runFile(argv[1]);
+    } else {
+      runPrompt();
+    }
+    return 0;
+  } catch (const std::exception &e) {
+    std::cout << e.what();
   }
 }
 
 void runFile(std::string filePath) {
   std::ifstream file{filePath};
   if (!file.is_open()) {
-    std::cout << "poo";
+    throw Err::UsageException(Err::UsageException::Type::CanNotOpenFile);
   }
 }
 
-void runPrompt() {}
+void runPrompt() {
+  std::string input;
+
+  std::cout << "> ";
+
+  while (std::getline(std::cin, input)) {
+    std::cout << input << std::endl;
+    std::cout << "> ";
+  }
+}
